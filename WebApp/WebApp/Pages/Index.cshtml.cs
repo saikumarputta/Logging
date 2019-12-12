ccc28dbe45c3197ed215998c23ccc2b7464e9e86
saikumar <saikumarputta01@gmail.com>
2019-12-12 15:46:58 +0530

    ILogger_Commit

---
+++ WebApp/WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger _logger;
+        ////This is the standard way of capturing the category
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public IndexModel(ILoggerFactory factory)
+        {
+            _logger = factory.CreateLogger("DemoCatageory");
+        }
+
+        public void OnGet()
+        {
+            //Logging levels
+            _logger.LogTrace("This is log trace");
+            _logger.LogDebug("This is log debug");
+            _logger.LogInformation("This is the information of the log");
+            _logger.LogError("This is the log error");
+            _logger.LogCritical("This is the log critical");
+
+            ////Advanced login messages
+            //_logger.LogError("The server went down temporarily at {Time}", DateTime.UtcNow);
+            //try
+            //{
+            //    throw new Exception("you forgot to catch me");
+            //}
+            //catch (Exception ex)
+            //{
+            //    _logger.LogCritical(ex,"There was a bad exception at {Time}",DateTime.UtcNow);
+            //    throw;
+            //}
+        }
+    }
+    public class LoggingId
+    {
+        public const int Democode = 1001;
+    }
+}
---
+++ WebApp/WebApp/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("Your Application has Started");
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+          return Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((context,logging) =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+                    logging.AddDebug();
+                    logging.AddConsole();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        }
+    }
+}
